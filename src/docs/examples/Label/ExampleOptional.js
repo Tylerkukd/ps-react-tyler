48d1375cfe635f4c33993b64d9dcfa7e9f56626b
Tyler Meek <tyler@Eurofoods.local>
2017-11-17 12:52:40 +0000

    Atom commit

---
+++ src/docs/examples/Label/ExampleOptional.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'ps-react-tyler/Label';
+
+/** Optional Label */
+export default function ExampleOptional() {
+  return <Label htmlFor="test" label="test" />
+}
